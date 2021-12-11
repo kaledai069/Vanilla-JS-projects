21695d508f84f008a2d0acd6b531846512314983
kaledai069 <parzival.cooler@gmail.com>
2021-12-11 18:47:58 +0545

    swipping feature to mobile apps

--- NOKIA 3310 SNAKE/app.js
+++ NOKIA 3310 SNAKE/app.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', ()=>
 {
     const squares = document.querySelectorAll('.grid div');
     const score_board = document.querySelector('.score span');
+    const game_over_ele = document.getElementById('game_over');
+    const grid_area = document.getElementById('grid')
+
     const rand_num = Math.floor(Math.random() * squares.length);
     squares[rand_num].classList.add('snake');
 
@@ -12,7 +15,7 @@ document.addEventListener('DOMContentLoaded', ()=>
     let score = 0;
     let apple_present = false;
     let dir_locs = [];
-    let dir_change = false;
+    var snake_speed = 200;
 
     snake_body.push([snake_initial_pos, snake_dir]);
 
@@ -36,6 +39,17 @@ document.addEventListener('DOMContentLoaded', ()=>
         }
     }
 
+    const speed_increment_test = ()=>
+    {
+        if(score % 5 == 0)
+        {
+            console.log(snake_speed);
+            snake_speed -= 15;
+            clearInterval(timer);
+            start_snake_move_timer(snake_speed);
+        }
+    }
+
     const fruit_catch = ()=>
     {
         if(apple_present)
@@ -46,6 +60,7 @@ document.addEventListener('DOMContentLoaded', ()=>
                 squares[apple_pos[apple_index]].classList.remove('apple');
                 apple_pos.splice(apple_index, 1);
                 score++;
+                speed_increment_test();
                 score_board.textContent = score;
                 add_snake_block();
                 apple_present = false;
@@ -53,16 +68,8 @@ document.addEventListener('DOMContentLoaded', ()=>
         }
     }
 
-    const remove_snake_shade = (snake_block_index) =>
-    {
-        squares[snake_block_index].classList.remove('snake');
-    }
-
-    const append_snake_shade = (snake_block_index)=>
-    {
-        squares[snake_block_index].classList.add('snake');
-    }
-
+    const remove_snake_shade = (snake_block_index) =>{squares[snake_block_index].classList.remove('snake');}
+    const append_snake_shade = (snake_block_index) =>{squares[snake_block_index].classList.add('snake');}
     const snake_movement_key_logger = ( snake_block_index, movement_style )=>
     {
         
@@ -127,59 +134,95 @@ document.addEventListener('DOMContentLoaded', ()=>
                 }
             }
             if(del_dir) dir_locs.shift();
-            
         }
     }
 
-    document.addEventListener('keydown', (e)=>
+    const key_dir_handler = (opp_dir, right_dir)=>
     {
-        switch(e.key)
+        if(snake_body.length > 1)
+        {
+            if(JSON.stringify(snake_body[0][1]) != JSON.stringify(opp_dir))
+            {
+                snake_dir = right_dir;
+                dir_locs.push([snake_body[0][0], snake_dir]);
+            }
+        }
+        else 
+        {
+            snake_dir = right_dir;
+            dir_locs.push([snake_body[0][0], snake_dir]);
+        }
+    }
+
+    key_strokes = 
+    {
+        left_key: ()=> {key_dir_handler([1, 0], [-1, 0]);},
+        right_key:()=> {key_dir_handler([-1, 0], [1, 0]);},
+        up_key:   ()=> {key_dir_handler([0, -1], [0, 1]);},
+        down_key: ()=>{key_dir_handler([0, 1], [0, -1]);}
+    }
+
+    const event_handler = (key_data, swipe_type)=>
+    {
+        let key_data_map =
+        {
+            left: 'a',
+            right: 'd',
+            up: 'w',
+            down: 's'
+        }
+        let key_str_value = swipe_type ? key_data_map[key_data] : key_data;
+        switch(key_str_value)
         {
             case 'a':
-                snake_dir = [-1, 0];
-                dir_locs.push([snake_body[0][0], snake_dir]);
+                key_strokes['left_key']();
                 break;
             case 'd':
-                snake_dir = [1, 0];
-                dir_locs.push([snake_body[0][0], snake_dir]);
+                key_strokes['right_key']();
                 break;
             case 'w':
-                snake_dir = [0, 1];
-                dir_locs.push([snake_body[0][0], snake_dir]);
+                key_strokes['up_key']();
                 break;
             case 's':
-                snake_dir = [0, -1];
-                dir_locs.push([snake_body[0][0], snake_dir]);
+                key_strokes['down_key']();
                 break;
         }
-    })
-
-    document.addEventListener('swiped', (e)=>
+    }
+    document.addEventListener('keydown', (e)=>
     {
-        switch(e.detail.dir)
-        {
-            case 'left':
-                snake_dir = [-1, 0];
-                dir_locs.push([snake_body[0][0], snake_dir]);
-                break;
-            case 'right':
-                snake_dir = [1, 0];
-                dir_locs.push([snake_body[0][0], snake_dir]);
-                break;
-            case 'up':
-                snake_dir = [0, 1];
-                dir_locs.push([snake_body[0][0], snake_dir]);
-                break;
-            case 'down':
-                snake_dir = [0, -1];
-                dir_locs.push([snake_body[0][0], snake_dir]);
-                break;
-        }
+        event_handler(e.key, false);
     })
 
+    grid_area.addEventListener('swiped', (e)=>
+    {
+        event_handler(e.detail.dir, true);
+    })
+
+    const change_snake_shade = ()=>
+    {
+        snake_body.forEach( snake_block => 
+            {
+                squares[snake_block[0]].classList.add('orange');
+            })
+    }
+
+    const check_for_collision = ()=>
+    {
+        let snake_head_pos = snake_body[0][0];
+        for(let i = 1; i < snake_body.length; i++)
+        {
+            if(snake_body[i][0] == snake_head_pos)
+            {
+                clearInterval(timer);
+                game_over_ele.textContent = 'Game Over';
+                change_snake_shade();
+            }
+        }
+    }
     const snake_move = () =>
     {
         check_for_direction();
+        check_for_collision();
         for(let i = 0, len = snake_body.length; i < len; i++)
         {
             if(JSON.stringify(snake_body[i][1]) == JSON.stringify([1,0]))
@@ -215,6 +258,10 @@ document.addEventListener('DOMContentLoaded', ()=>
         }
     }
 
-    setInterval(function(){snake_move()}, 100);
+    const start_snake_move_timer = (speed)=>
+    {
+        timer = setInterval(function(){snake_move()}, speed);
+    }
+    start_snake_move_timer(snake_speed);
     setInterval(function(){random_apple_generator()}, 500)
 })
