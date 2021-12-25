63b9abda16a9d72370369f4ee86ff10fbf29cc6f
kaledai069 <parzival.cooler@gmail.com>
2021-12-25 13:31:28 +0545

    Block points stuck

--- Tetris/app.js
+++ Tetris/app.js
@@ -141,9 +141,8 @@ document.addEventListener('DOMContentLoaded', ()=>
     let active_shape_color;
     let actual_start_pos;
     let active_shape_bool_copy = new Array;
-
     let rand_tetris_obj;
-    let present_selected_obj = -1;
+    let present_selected_obj;
     let ran_active_shape;
 
     function initializers()
@@ -160,14 +159,13 @@ document.addEventListener('DOMContentLoaded', ()=>
         {
             actual_start_pos = random_start_pos - 10 * (active_shape.length - 1);
         }
-        console.log(active_shape);
         mask_resolver(actual_start_pos);
         mask_mapper(active_shape_color, true);
     }
 
     initializers();
 
-    function down_bounday_checker()
+    function down_boundary_checker()
     {
         let last_row = active_shape[active_shape.length - 1];
         let last_row_pos = 0;
@@ -183,19 +181,91 @@ document.addEventListener('DOMContentLoaded', ()=>
         {
             return true;
         }
-        else if(typeof(last_row_pos) == 'undefined' || last_row == 0)
-        {
-            return false;
-        }
         else
         {
             return false;
         }
     }
 
+    function edge_boundary_checker()
+    {
+        for(let i = 0; i < active_shape.length; i++)
+        {
+            for(let j = 0; j < active_shape[i].length; j++)
+            {
+                if(typeof(active_shape[i][j]) == 'number')
+                {
+                    let temp_box_no = active_shape[i][j] + 10;
+                    if(all_squares[temp_box_no].classList.contains('taken'))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    function clear_n_shift_block(start_value, row_value)
+    {
+        // clear all the block
+        for(let i = start_value; i < start_value + 10; i++)
+        {
+            all_squares[i].classList.remove('taken');
+            let color_list = ['red', 'sky', 'blue', 'yellow', 'white'];
+
+            color_list.forEach( color => {
+                if(all_squares[i].classList.contains(color))
+                    all_squares[i].classList.remove(color)
+            });
+        }
+
+        start_value -= 10;
+        // shift the blocks
+        for(let i = row_value - 1; i >= 0; i--)
+        {
+            for(let j = start_value; j < start_value + 10; j++)
+            {
+                if(all_squares[j].classList.contains('taken'))
+                {
+                    let color = all_squares[j].classList[1];
+                    console.log(color);
+                    all_squares[j].classList.remove('taken');
+                    all_squares[j].classList.remove(color);
+                    all_squares[j+10].classList.add('taken', color);
+                }
+            }
+            start_value -= 10;
+        }
+
+    }
+
+    function check_for_points()
+    {
+        let starting_value = 190;
+        let block_count = 0;
+
+        for(let i = 19; i >= 0; i--)
+        {
+            for(let j = starting_value; j < starting_value + 10; j++)
+            {
+                if(all_squares[j].classList.contains('taken'))
+                {
+                    block_count++;
+                }
+            }
+            if(block_count == 10)
+            {
+                clear_n_shift_block(starting_value, i);
+            }
+            block_count = 0;
+            starting_value -= 10;
+        }
+    }
+
     function re_mapper(value)
     {
-        if(!down_bounday_checker())
+        if(!down_boundary_checker() && !edge_boundary_checker())
         {
             mask_mapper(active_shape_color, false);
             actual_start_pos += value;
@@ -214,7 +284,105 @@ document.addEventListener('DOMContentLoaded', ()=>
                     }
                 }
             }
+           
             initializers();
+        }
+        check_for_points();
+    }
+
+    function left_movement_possible()
+    {
+        for(let i = 0; i < active_shape.length; i++)
+        {
+            if(typeof(active_shape[i][0]) == 'number')
+            {
+                if(active_shape[i][0] % 10 == 0 || all_squares[active_shape[i][0] - 1].classList.contains('taken'))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function right_movement_possible()
+    {
+        for(let i = 0; i < active_shape.length; i++)
+        {
+            if(typeof(active_shape[i][active_shape[i].length-1]) == 'number')
+            {
+                if((active_shape[i][active_shape[i].length-1]) % 10 == 9 || all_squares[active_shape[i][active_shape[i].length-1] + 1].classList.contains('taken'))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    function rotation_collision()
+    {
+        let next_shape = present_selected_obj['structure'][(ran_active_shape + 1) % present_selected_obj['structure'].length]; 
+        let temp_next_shape = next_shape.map(x => x.slice());
+        let temp_start_pos = actual_start_pos;
+        for(let i = 0; i < temp_next_shape.length; i++)
+        {
+            let row_len = temp_next_shape[i].length;
+            for(let j = 0; j < row_len; j++)
+            {
+                if(temp_start_pos >= 0 && temp_start_pos != 200)
+                {
+                    temp_next_shape[i][j] = temp_start_pos; 
+                }
+                temp_start_pos++;
+            }
+            temp_start_pos = temp_start_pos + 10 - row_len;
+        }
+        for(let i = 0; i < temp_next_shape.length; i++)
+        {
+            for(let j = 0; j < temp_next_shape[i].length; j++)
+            {
+                if(typeof(temp_next_shape[i][j]) == 'number')
+                {
+                    if(all_squares[temp_next_shape[i][j]].classList.contains('taken'))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    function shift_boundary_collision()
+    {
+        let next_shape = present_selected_obj['structure'][(ran_active_shape + 1) % present_selected_obj['structure'].length]; 
+        let temp_start_pos = actual_start_pos;
+        return temp_start_pos % 10 + next_shape[0].length > 10;
+    }
+
+    function rotate_shape()
+    {
+        if(!rotation_collision() && !shift_boundary_collision() && actual_start_pos >= 0)
+        {
+            mask_mapper(active_shape_color, false);
+            ran_active_shape = (ran_active_shape + 1) % present_selected_obj['structure'].length;
+            let next_shape = present_selected_obj['structure'][ran_active_shape]; 
+            active_shape = next_shape.map(x => x.slice());
+            active_shape_bool_copy = next_shape.map(x => x.slice());
+            mask_resolver(actual_start_pos);
+            mask_mapper(active_shape_color, true);
+        }
+    }
+
+    function all_the_way_down()
+    {
+        while(!down_boundary_checker() && !edge_boundary_checker())
+        {
+            mask_mapper(active_shape_color, false);
+            actual_start_pos += 10;
+            mask_resolver(actual_start_pos);
+            mask_mapper(active_shape_color, true);
         }
     }
 
@@ -223,10 +391,18 @@ document.addEventListener('DOMContentLoaded', ()=>
         switch(e.key)
         {
             case 'a':
-                re_mapper(-1);                
+                if(left_movement_possible())
+                    re_mapper(-1);                
                 break;
             case 'd':
-                re_mapper(1);   
+                if(right_movement_possible())
+                    re_mapper(1);   
+                break;
+            case ' ':
+                rotate_shape();
+                break;
+            case 's':
+                all_the_way_down();
                 break;
         }
     })
@@ -234,5 +410,5 @@ document.addEventListener('DOMContentLoaded', ()=>
     setInterval(()=>
     {
         re_mapper(10);
-    }, 200)
+    }, 300)
 })
